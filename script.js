c79721583a622f2d4f033753b9b8500aca7544de
kvantalianitekla <248906567+kvantalianitekla@users.noreply.github.com>
2026-02-13 16:22:59 +0400

    დავალება 2 დასრულებულია

--- script.js
+++ script.js
@@ -28,7 +28,25 @@
 //   age = Number(prompt("Enter your age again:"));
 // }
 
-// console.log("name:", name);
-// console.log("age:", age);
-
 //----------------------------------------------------------------
+
+// 2️ მომხმარებლის სტატუსის განსაზღვრა
+// (if / else, შედარების ოპერატორები)
+// ასაკის მიხედვით განსაზღვრე სტატუსი:
+// •	ასაკი < 18 → Teen User
+// •	ასაკი 18 – 60 → Active User
+// •	ასაკი > 60 → Senior User
+
+let status;
+
+if (age < 18) {
+  status = "Teen User";
+} else if (age <= 60) {
+  status = "Active User";
+} else {
+  status = "Senior User";
+}
+
+console.log("name:", name);
+console.log("age:", age);
+console.log("status:", status);
