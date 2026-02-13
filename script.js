6f88e7cefbbcfbfef29ff7141eccad6c5dc6542d
kvantalianitekla <248906567+kvantalianitekla@users.noreply.github.com>
2026-02-13 17:30:07 +0400

    დავალება 4 დასრულებულია

--- script.js
+++ script.js
@@ -12,21 +12,21 @@
 // •	გადაამოწმე მონაცემების ტიპები typeof-ით
 // ❗ თუ რომელიმე პირობა არ სრულდება → ისევ ჰკითხე შესაბამისი მონაცემი (while)
 
-// let name = prompt("Enter your name:");
+let name = prompt("Enter your name:");
 
-// while (name === null || name.trim() === "" || name.trim().length < 3) {
-//   alert("სახელი არ უნდა იყოს ცარიელი. სახელი უნდა იყოს მინიმუმ 3 სიმბოლო");
-//   name = prompt("Enter your name again:");
-// }
+while (name === null || name.trim() === "" || name.trim().length < 3) {
+  alert("სახელი არ უნდა იყოს ცარიელი. სახელი უნდა იყოს მინიმუმ 3 სიმბოლო");
+  name = prompt("Enter your name again:");
+}
 
-// name = name.trim();
+name = name.trim();
 
-// let age = Number(prompt("Enter your age:"));
+let age = Number(prompt("Enter your age:"));
 
-// while (typeof age !== "number" || isNaN(age) || age < 1 || age > 120) {
-//   alert("ასაკი უნდა იყოს 1–120 შორის");
-//   age = Number(prompt("Enter your age again:"));
-// }
+while (typeof age !== "number" || isNaN(age) || age < 1 || age > 120) {
+  alert("ასაკი უნდა იყოს 1–120 შორის");
+  age = Number(prompt("Enter your age again:"));
+}
 
 //----------------------------------------------------------------
 
@@ -37,19 +37,19 @@
 // •	ასაკი 18 – 60 → Active User
 // •	ასაკი > 60 → Senior User
 
-// let status;
+let status;
 
-// if (age < 18) {
-//   status = "Teen User";
-// } else if (age <= 60) {
-//   status = "Active User";
-// } else {
-//   status = "Senior User";
-// }
+if (age < 18) {
+  status = "Teen User";
+} else if (age <= 60) {
+  status = "Active User";
+} else {
+  status = "Senior User";
+}
 
-// console.log("name:", name);
-// console.log("age:", age);
-// console.log("status:", status);
+console.log("name:", name);
+console.log("age:", age);
+console.log("status:", status);
 
 //------------------------------------------------------------------
 
@@ -94,3 +94,36 @@ if (red < 120 && green < 120 && blue < 120) {
 }
 
 console.log("Theme:", theme);
+
+//--------------------------------------------------------------------
+
+// 4️ პროფილის ბარათის შექმნა
+// (document.write, String concatenation)
+// document.write-ით შექმენი User Profile Card, რომელიც შეიცავს:
+// •	მომხმარებლის სახელს
+// •	ასაკს
+// •	სტატუსს
+// •	RGB ფერს (ტექსტურად)
+// •	Theme ტიპს
+// ვიზუალური მოთხოვნები:
+// •	ტექსტის ფერი → გენერირებული RGB
+// •	ჩარჩო → იგივე ფერი
+// •	მარტივი padding და margin (inline style ან კლასი გაუწერე და css-ში გასტილე)
+
+document.write(`
+  <div
+    style="
+  color: ${rgbColor};
+  border: 2px solid ${rgbColor};
+  padding: 10px;
+  margin: 10px;
+  
+  "
+  >
+    <p>Name: ${name}</p>
+    <p>Age: ${age}</p>
+    <p>Status: ${status}</p>
+    <p>RGB color: ${rgbColor}</p>
+    <p>Theme: ${theme}</p>
+  </div>
+`);
